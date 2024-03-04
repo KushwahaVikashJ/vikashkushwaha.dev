58a0c14cb65287c5653e1362ebf482e51af71fa2
KushwahaVikashJ <kushwahavicky743@gmail.com>
2024-03-04 20:09:58 +0530

    updated meta tags

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -11,7 +11,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
   const meta = {
     title: "Vikash Kushwaha - Full Stack Developer",
     description: `I've been developing websites for 3 years straight. Get in touch with me to know more.`,
-    image: "/avatar.png",
+    image: "/avatar.jpeg",
     type: "website",
     ...customMeta,
   };
--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ import userData from "@constants/data";
 export default function Home({ repositories }) {
   return (
     <ContainerBlock
-      title="Vikash Kushwaha - Developer, Writer, Creator"
-      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+      title="Vikash Kushwaha - Full Stack Developer"
+      description="Experienced software engineer with a focus on building engaging and accessible digital experiences."
     >
       <Hero />
       <FavouriteProjects />
