b5b22689da44379222254e4bfdbf29e12751c62d
KushwahaVikashJ <kushwahavicky743@gmail.com>
2024-03-04 17:52:22 +0530

    first commit

---
+++ constants/data.js
@@ -0,0 +1,74 @@
+const userData = {
+  githubUsername: "KushwahaVikashJ",
+  name: "Vikash Kushwaha",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.jpeg",
+  email: "kushwahavikas499@gmail.com",
+  phone: "+91 7435977621",
+  address: "Surat, Gujarat, India.",
+  projects: [
+    {
+      title: "Teckle",
+      link: "#",
+      imgUrl: "/teckle.png",
+    },
+    {
+      title: "Tr Capital",
+      link: "#",
+      imgUrl: "/tr-capital.png",
+    },
+    {
+      title: "Dev Connector",
+      link: "#",
+      imgUrl: "/dev-connector.png",
+    },
+  ],
+  about: {
+    title:
+      "Experienced software engineer with a focus on building engaging and accessible digital experiences.",
+    description: [
+      `I'm Vikash Kushwaha, a seasoned software engineer specializing in crafting engaging digital solutions. With extensive experience in React and Next.js, I've led projects at Whitechalk, enhancing user engagement on the Teckle platform. At Logicwind, I focused on optimizing performance as a React Developer, while my internship at Benchwork Technologies sharpened my frontend skills. Armed with a Bachelor's in Computer Science, I'm proficient in JavaScript, TypeScript, HTML, and CSS, with expertise in React, Next.js, Node.js, and Tailwind CSS. Beyond coding, I enjoy reading and exploring the latest web-series. Passionate about innovation, I'm dedicated to creating impactful digital experiences.`,
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Software Engineer",
+      company: "Whitechalk",
+      year: "07/2023 – Present",
+      companyLink: "https://www.whitechalk.xyz/",
+      desc: "Managed projects involving both React and Next.js frameworks, delivering scalable and efficient web applications",
+    },
+    {
+      title: "React Developer",
+      company: "Logicwind",
+      year: "09/2021 - 06/2023",
+      companyLink: "https://www.logicwind.com/",
+      desc: "Spearheaded the development of an admin video platform and a web platform for video services, leveraging React for frontend functionalities",
+    },
+    {
+      title: "Frontend Intern",
+      company: "Benchwork Technologies",
+      year: "11/2020 - 09/2021",
+      companyLink: "https://www.benchworktechnologies.com/",
+      desc: "Contributed to the development of a doctor appointment platform, focusing on frontend functionalities and user interface enhancements.",
+    },
+    {
+      title: "Graduation",
+      company: "Lovely Professional University",
+      year: "2017-2021",
+      companyLink: "https://lpu.in",
+      desc: "Major in Computer Science and Engineering with a CGPA of 7.88. Nobody asks this but it's okay.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1fakZOFnG-CnFFNIZautCTtEoh5UHsObW/view?usp=sharing",
+  socialLinks: {
+    discord: "https://discord.com/channels/@me/1091736082787356683",
+    linkedin: "https://www.linkedin.com/in/kushwaha-vikash-323144170/",
+    github: "https://github.com/KushwahaVikashJ",
+  },
+};
+
+export default userData;
