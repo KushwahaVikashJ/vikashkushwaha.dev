bd50aa86cf26ee98a7bff74a8a6541c4c9b65e2e
KushwahaVikashJ <kushwahavicky743@gmail.com>
2024-05-02 17:27:31 +0530

    updated meta tags

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -23,11 +23,11 @@ export default function ContainerBlock({ children, ...customMeta }) {
         <meta content={meta.description} name="description" />
         <meta
           property="og:url"
-          content={`https://www.vikashkushwaha.dev/${router.asPath}`}
+          content={`https://www.vikashkushwaha.dev${router.asPath}`}
         />
         <link
           rel="canonical"
-          href={`https://www.vikashkushwaha.dev/${router.asPath}`}
+          href={`https://www.vikashkushwaha.dev${router.asPath}`}
         />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Vikash Kushwaha" />
